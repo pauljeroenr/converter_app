d0a0eaa41adb34e99b881212acb16f7bc5c365b2
PJR <paul_jeroen16@hotmail.com>
2020-08-29 23:30:31 +0200

    Add a dynamic dropdown for more metric

--- App.js
+++ App.js
@@ -1,17 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
-import { Text, View, Picker, TextInput, TouchableOpacity } from 'react-native';
+import { Text, View, Picker, TextInput, TouchableOpacity, Alert } from 'react-native';
 import { Header} from 'react-native-elements';
 import { styles } from './styles.js';
 import { fahrenheit2celsius, inch2cm, cup2ml, yard2m, ounce2ml } from "./converter_functions.js";
+import { supported_units_dict } from "./supported_units.js"
 
 
 const right_arrow = "\u2192";
 
 export default function App() {
   const [selectedValue, setSelectedValue] = useState("temp");
+  const [selectedUnit, setSelectedUnit] = useState('')
   const [value, onChangeText] = useState('');
-  const [convValue, onCalcText] = useState('')
+  const [convValue, onCalcText] = useState('');
+
   return (
     <View style = {{flex: 1, justifyContent: 'flex-start'}}>
       <Header
@@ -22,13 +25,10 @@ export default function App() {
       <View style = {{flex: 0.3}}/>
       <View style = {styles.container}>
         <Picker
-          prompt="Choose Unit to Convert"
+          prompt="Choose Category to Convert"
           selectedValue={selectedValue}
           style={{ height: 50, width: 200 }}
           mode="dialog"
-          itemStyle={{
-            backgroundColor: "#C38D9E"            
-          }}
           onValueChange={(itemValue, itemIndex) => {setSelectedValue(itemValue);
                                                     onChangeText("");
                                                     onCalcText("");
@@ -39,6 +39,20 @@ export default function App() {
           <Picker.Item label="Volume" value="vol"/>
           <Picker.Item label="Size" value="size"/>
           <Picker.Item label="Distance" value="dist"/>
+        </Picker>
+        <Picker
+          prompt="Choose Unit to Convert"
+          selectedValue={selectedUnit}
+          style={{ height: 50, width: 200 }}
+          mode="dialog"
+          onValueChange={(itemValue, itemIndex) => {setSelectedUnit(itemValue);
+                                                    onChangeText("");
+                                                    onCalcText("");
+          }}
+        >
+          {supported_units_dict[selectedValue].map(unit => (
+            <Picker.Item label={unit.label} value={unit.value}/>
+          ))}
         </Picker>
         <View style = {styles.inputField}>
           <TextInput 
---
+++ supported_units.js
@@ -0,0 +1,13 @@
+export const supported_units_dict = {
+    "temp": [{label: "Fahrenheit - C°", value: "fahrenheit"}],
+    "cook": [{label: "Cup - ML", value: "cup"},
+             {label: "Tablespoon - ML", value: "tbsp"},
+             {label: "Teaspoon - ML", value: "teasp"}],
+    "vol":  [{label: "Ounze - ML", value: "oz"},
+             {label: "Gallon - L", value: "gallon"},
+             {label: "Pint - ML", value: "pint"}],
+    "size": [{label: "Inch - CM", value: "inch"},
+             {label: "Feet - CM", value: "feet"}],
+    "dist": [{label: "Mile - KM", value: "mile"},
+             {label: "Yard - M", value: "yard"}]
+}
